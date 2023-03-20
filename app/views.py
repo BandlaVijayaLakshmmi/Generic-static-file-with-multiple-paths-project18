c57bd724cde22564d11855b8379d805f1e49c92b
BandlaVijayaLakshmi <bandlavijayalakshmi2001@gmail.com>
2023-03-20 12:33:16 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def form1(request):
+    return render(request,'form1.html')
+def form2(request):
+    return render(request,'form2.html')
+def form3(request):
+    return render(request,'form3.html')
+def form4(request):
+    return render(request,'form4.html')
